cd373f031117554bc535a1f1ef5a62b0b425ca34
Indira Mendoza <indira.cursoadtek@gmail.com>
2024-03-14 20:54:50 -0600

    "Ejemplos de Var y Tipo de datos"

---
+++ 05-Var/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            Console.WriteLine("El tipo de dato de i es:{0}",i.GetType() );
+
+            var str = "Hellow Word!!";
+            Console.WriteLine("El tipo de dato de str es:{0}", str.GetType());
+
+            var dbl = 100.50d;
+            Console.WriteLine("El tipo de dato dbl es: {0}",dbl.GetType());
+
+            var isValid = true;
+            Console.WriteLine("El tipo de datos isValid es {0}:",isValid.GetType());
+
+            var ano = new { name = "Steve", lasname ="Perez" };//Este es un anonimo
+            Console.WriteLine("El tipo de datos ano es: {0}",ano.GetType());
+
+            var arr = new[] { 1, 10, 20, 30 };
+            Console.WriteLine("El tipo de dato arr es: {0}",arr.GetType());
+
+            var file = new FileInfo("MyFile");
+            Console.WriteLine("El tipo de dato file es:{0}",file.GetType());
+
+            //var j; //Las variables tipadas deben inicializarse
+            //j = 100;
+
+            //var h = 100, j = 200, k = 300;
+
+            var h = 100;
+            var j = 200;
+            var k = 300;
+            Console.WriteLine(h);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+            //Utiizando Var en un ciclo FOR
+            for (var l = 0; l < 10; l++)
+            {
+
+                Console.WriteLine(i);
+            }
+
+            //string collection
+            IList<String> stringList = new List<String>();
+            {
+               stringList.Add("C# Tutorial");
+               stringList.Add("VB.NET Tutorial");
+               stringList.Add("Learn C++");
+               stringList.Add("MVC Tutorial");
+               stringList.Add("Java");
+                
+            }
+
+
+            //Utilizando Var en un expresion LINQ
+             var result = from s in stringList 
+                          where s.Contains("Tutorial")
+                          select s;
+
+            foreach (var s in result)
+            {
+                Console.WriteLine(s);
+            }
+                
+            
+            Console.ReadLine();
+        }
+        //No se puede utilizar la palabra reservada VAR en parametros
+        //public static void Display(var parametro) {
+        //    Console.WriteLine(parametro);
+        //}
+    }
+}
---
+++ 06-TipoDatos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_TipoDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numero = 100;
+            Console.WriteLine("El valor de la variable numerò {0} ",numero);
+
+            sbyte numero2 = 127;
+            Console.WriteLine("El numero de la variable numero {0}", numero2);
+
+            uint numero3 = 1000u;
+            Console.WriteLine("El numero de la variable numero {0}", numero3);
+
+            long numero4 = 1000L;
+            Console.WriteLine("El numero de la variable numero {0}", numero4);
+
+            DateTime fecha = new DateTime(9999, 12, 31, 23, 59, 59);
+            Console.WriteLine("El numero de la variable numero {0}", fecha);
+
+           object miObject = new object();
+
+            //Inicializacion de variables
+            int i = default;
+            Console.WriteLine("El valor de la variable i {0}", i);
+
+            //Conversion implicita
+            int j = 345;
+            float f = j;
+            Console.WriteLine("El valor de la variable f {0}",f);
+
+            //Conversion Explicita
+            int h = 100;
+            uint u = (uint)h;
+            Console.WriteLine(u);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
